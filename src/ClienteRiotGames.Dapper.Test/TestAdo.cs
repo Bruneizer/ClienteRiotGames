6d8eb0d4f6367fb5c836c7e615e7292e028e328c
Armoaaa <164027531+Armoaaa@users.noreply.github.com>
2025-02-27 06:51:41 -0300

    reset final

---
+++ src/ClienteRiotGames.Dapper.Test/TestAdo.cs
@@ -0,0 +1,7 @@
+global using System.Data;
+global using MySqlConnector;
+global using Microsoft.Extensions.Configuration;
+global using ClienteRiotGames.Dapper;
+global using ClienteRiotGames.Core;
+global using Xunit;
+global using Dapper;
--- src/ClienteRiotGames.Dapper.Test/obj/Debug/net8.0/ClienteRiotGames.Dapper.Test.AssemblyInfo.cs
+++ src/ClienteRiotGames.Dapper.Test/obj/Debug/net8.0/ClienteRiotGames.Dapper.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteRiotGames.Dapper.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cce9bc2bdf6823efdb94c2e1826211fb688ca04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e159bffefd889eb1b1e43a69eeaa9ff540924a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteRiotGames.Dapper.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClienteRiotGames.Dapper.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
